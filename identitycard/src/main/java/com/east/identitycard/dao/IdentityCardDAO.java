a1aed53c19b61193f0ad046a6ff46192f1937b2c
Nolan <Nolan.Cheng@bleum.com>
2015-06-15 17:52:09 +0800

    create ResidenceAddressDAO and modify IdentityCardDAO IdentityCard ResidenceAddress

--- identitycard/src/main/java/com/east/identitycard/dao/IdentityCardDAO.java
+++ identitycard/src/main/java/com/east/identitycard/dao/IdentityCardDAO.java
@@ -1,18 +1,15 @@
 package com.east.identitycard.dao;
 
-import java.util.List;
-
 import com.east.identitycard.entity.IdentityCard;
 
 public interface IdentityCardDAO {
 
 	public void saveIdentityCard(IdentityCard identityCard);
 
-	public List<IdentityCard> getIdentityCards();
-
-	public IdentityCard getIdentityCard();
+	public IdentityCard getIdentityCard(int id);
 
 	public void updateIdentityCard(IdentityCard identityCard);
 
 	public void deleteIdentityCard(IdentityCard identityCard);
+	
 }
---
+++ identitycard/src/main/java/com/east/identitycard/dao/ResidenceAddressDAO.java
@@ -0,0 +1,15 @@
+package com.east.identitycard.dao;
+
+import com.east.identitycard.entity.ResidenceAddress;
+
+public interface ResidenceAddressDAO {
+	
+	public void saveResidenceAddress(ResidenceAddress residenceAddress);
+	
+	public void updateResidenceAddress(ResidenceAddress residenceAddress);
+	
+	public ResidenceAddress getResidenceAddress(int id);
+	
+	public void deleteResidenceAddress(ResidenceAddress residenceAddress);
+
+}
